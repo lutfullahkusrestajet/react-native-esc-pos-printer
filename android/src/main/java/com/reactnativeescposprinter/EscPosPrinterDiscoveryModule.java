3caaa16e65df80989e5d27e9a06cae2f103b1d09
lutfullahkusrestajet <106922799+lutfullahkusrestajet@users.noreply.github.com>
2023-12-04 19:48:54 +0300

    Update EscPosPrinterDiscoveryModule.java

--- android/src/main/java/com/reactnativeescposprinter/EscPosPrinterDiscoveryModule.java
+++ android/src/main/java/com/reactnativeescposprinter/EscPosPrinterDiscoveryModule.java
@@ -39,17 +39,13 @@ import com.google.android.gms.common.api.CommonStatusCodes;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.ConnectionResult;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
+import java.util.ArrayList;
 
 @ReactModule(name = EscPosPrinterDiscoveryModule.NAME)
 public class EscPosPrinterDiscoveryModule extends ReactContextBaseJavaModule implements ActivityEventListener {
 
   private Context mContext;
-  
-  private JSONArray mPrinterList = null;
-
+  private ArrayList<DeviceInfo> mPrinterList = null;
   private final ReactApplicationContext reactContext;
 
   public static final String NAME = "EscPosPrinterDiscovery";
@@ -138,13 +134,10 @@ public class EscPosPrinterDiscoveryModule extends ReactContextBaseJavaModule imp
       return;
     }
 
-
-      LocationRequest locationRequest = 
-     new LocationRequest.Builder(
-      LocationRequest.PRIORITY_HIGH_ACCURACY,
-      10000
-     ).build();
-
+    LocationRequest locationRequest = LocationRequest.create();
+    locationRequest.setInterval(10000);
+    locationRequest.setFastestInterval(5000);
+    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
 
     SettingsClient client = LocationServices.getSettingsClient(reactContext);
@@ -183,13 +176,13 @@ public class EscPosPrinterDiscoveryModule extends ReactContextBaseJavaModule imp
   }
 
   private void sendEvent(ReactApplicationContext reactContext, String eventName, @Nullable WritableArray params) {
-    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName,params);
+    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
   }
 
   @ReactMethod
   private void startDiscovery(final ReadableMap paramsMap, Promise promise) {
     FilterOption mFilterOption = getFilterOptionsFromParams(paramsMap);
-    mPrinterList = new JSONArray();
+    mPrinterList = new ArrayList<DeviceInfo>();
 
     try {
       Discovery.start(mContext, mFilterOption, mDiscoveryListener);
@@ -246,54 +239,32 @@ public class EscPosPrinterDiscoveryModule extends ReactContextBaseJavaModule imp
     return mFilterOption;
   }
 
-   private DiscoveryListener mDiscoveryListener = new DiscoveryListener() {
+  private DiscoveryListener mDiscoveryListener = new DiscoveryListener() {
     @Override
     public void onDiscovery(final DeviceInfo deviceInfo) {
 
       UiThreadUtil.runOnUiThread(new Runnable() {
         @Override
-        public synchronized void run() {
-
-        JSONObject printerData = new JSONObject();
-
-try{
-          printerData.put("target", deviceInfo.getTarget());
-          printerData.put("deviceName", deviceInfo.getDeviceName());
-          printerData.put("ipAddress", deviceInfo.getIpAddress());
-          printerData.put("macAddress", deviceInfo.getMacAddress());
-          printerData.put("bdAddress", deviceInfo.getBdAddress());
-
-          mPrinterList.put(printerData);
-
- } catch (JSONException e) {
-         throw new RuntimeException(e);
-     }
+        public synchronized void run() {         
+          mPrinterList.add(deviceInfo);
 
 
+        WritableArray result=Arguments.createArray();
 
-          WritableArray writableArray = Arguments.createArray();
+        for (DeviceInfo deviceInfo : mPrinterList) {
 
-           for (int i = 0; i < mPrinterList.length(); i++) {
+         WritableMap printerData = Arguments.createMap();
 
-                 try{
-                    WritableMap printerData2 = Arguments.createMap();
+          printerData.putString("target", deviceInfo.getTarget());
+          printerData.putString("deviceName", deviceInfo.getDeviceName());
+          printerData.putString("ipAddress", deviceInfo.getIpAddress());
+          printerData.putString("macAddress", deviceInfo.getMacAddress());
+          printerData.putString("bdAddress", deviceInfo.getBdAddress());
 
-                    JSONObject jsonObject= mPrinterList.getJSONObject(i);
-
-                    printerData2.putString("target", jsonObject.getString("target"));
-                    printerData2.putString("deviceName", jsonObject.getString("deviceName"));
-                    printerData2.putString("ipAddress", jsonObject.getString("ipAddress"));
-                    printerData2.putString("macAddress", jsonObject.getString("macAddress"));
-                    printerData2.putString("bdAddress", jsonObject.getString("bdAddress"));
-                   writableArray.pushMap(printerData2);
-                 } catch (JSONException e) {
-         throw new RuntimeException(e);
-     }
-             
-            }
-
-          sendEvent(reactContext, "onDiscovery", writableArray);
-
+          result.pushMap(printerData);
+          }
+        
+          sendEvent(reactContext, "onDiscovery", result);
         }
       });
     }
